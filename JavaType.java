d0ac57a3b3567a0a5f5bd2818ad0098227b859bf
NiplusM <31923642+NiplusM@users.noreply.github.com>
2017-09-13 18:10:44 +0300

    Update JavaType.java

--- JavaType.java
+++ JavaType.java
@@ -3,13 +3,31 @@ public class JavaTypes {
   public static void main(String... args) {
     System.out.println("Сумма цифр 327: " + summ(327) + "\nОкругленное до целого число 1.276: " + roundValue(1.276));
   }
-  
+
   public static int summ(int value) {
     //Тут ваш код
+    int summ = 0;
+    int currentDigit = 0;
+    int ost = value;
+
+    while (ost != 0) {
+    	currentDigit = ost % 10;
+    	summ += currentDigit;
+    	ost = (ost - currentDigit) / 10;
+    }
+    
+    return summ;
   }
 
 
  public static int roundValue(double value) {
     //Тут ваш код
+    if ((value - (int) value) < 0.5) {
+    	return (int)(value - (value - (int)value));
+    } else {
+    	return (int)(value - (value - (int)value) + 1);
+    }
+
   }
 }
+
